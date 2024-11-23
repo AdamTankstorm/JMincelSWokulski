a1d0b6251ee2b468645d3ac72267c15f742d99bd
ignacywodecki <ignacywodecki@DOM.SCI.EDU.PL>
2024-11-23 07:10:46 +0100

    targowanie

--- SklepGalanteryjny/Assets/Scripts/Inventory.cs
+++ SklepGalanteryjny/Assets/Scripts/Inventory.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using System;
 public class Inventory : MonoBehaviour
 {
+    
     public EventHandler onItemsChanged;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public List<Item> ItemsList;
---
+++ SklepGalanteryjny/Assets/Scripts/haggling.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class haggling : MonoBehaviour
+{
+    public Slider slider;
+    public Image gradientImage;
+    private Texture2D gradientTexture;
+    private Camera camera;
+    public int greenStart;
+    
+    void OnEnable()
+    {
+        CreateGradient();
+        camera= FindAnyObjectByType(typeof(Camera)) as Camera;
+    }
+
+    private void Update()
+    {
+        slider.value = Mathf.PingPong(Time.time / Time.fixedDeltaTime, 100);
+        if (Input.GetButton("Fire1"))
+        {
+            if (slider.value >= greenStart && slider.value < greenStart + 10) { 
+
+            }
+            else if ((slider.value >= greenStart - 20 && slider.value < greenStart) || (slider.value >= greenStart + 10 && slider.value < greenStart + 20 + 10)) // Red bar surrounding green
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+    void CreateGradient()
+    {
+        int totalLength = 100;
+        int greenLength = 10; // Thin green bar
+        int redLength = 20;   // Red bar on both sides
+
+        // Random start position for the green bar, ensuring there's space for red on both sides
+        greenStart = Random.Range(redLength, totalLength - greenLength - redLength);
+
+        gradientTexture = new Texture2D(totalLength, 1);
+        gradientTexture.wrapMode = TextureWrapMode.Clamp;
+
+        Color[] colors = new Color[totalLength];
+
+        for (int i = 0; i < totalLength; i++)
+        {
+            if (i >= greenStart && i < greenStart + greenLength) // Green bar
+            {
+                colors[i] = Color.green;
+            }
+            else if ((i >= greenStart - redLength && i < greenStart) || (i >= greenStart + greenLength && i < greenStart + greenLength + redLength)) // Red bar surrounding green
+            {
+                colors[i] = Color.red;
+            }
+            else // Rest filled with yellow
+            {
+                colors[i] = Color.yellow;
+            }
+        }
+
+        gradientTexture.SetPixels(colors);
+        gradientTexture.Apply();
+
+        gradientImage.sprite = Sprite.Create(gradientTexture, new Rect(0, 0, gradientTexture.width, gradientTexture.height), new Vector2(0.5f, 0.5f));
+    }
+}
